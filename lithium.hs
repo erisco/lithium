0e70cad8e232987f7037e5806bd7adf0385700df
Eric Brisco <eric.brisco@gmail.com>
2014-03-13 21:42:27 -0400

    Removed unnecessary qualifiers for Reactive and Context.

--- lithium.hs
+++ lithium.hs
@@ -20,22 +20,22 @@ import qualified FRP.Sodium as NaP
 import FRP.Sodium (Plain)
 
 -- Generalised type over behaviours and events.
-newtype Behaviour r a = Behaviour (Na.Reactive r (Na.Behaviour r a))
+newtype Behaviour r a = Behaviour (Reactive r (Na.Behaviour r a))
 
-instance (Na.Context r) => Functor (Behaviour r) where
+instance (Context r) => Functor (Behaviour r) where
   fmap f (Behaviour r) = Behaviour $ fmap (fmap f) r
 --
 
-instance (Na.Context r) => Applicative (Behaviour r) where
+instance (Context r) => Applicative (Behaviour r) where
   pure = wrap . fmap fst . Na.newBehaviour
   x <*> y = wrap $ (liftA2 (<*>)) (unwrap x) (unwrap y)
 --
 
 
 -- An attempt to make a Monad instance but it falls short.
---instance (Na.Context r) => Monad (Behaviour r) where
+--instance (Context r) => Monad (Behaviour r) where
 --  return = pure
---  -- gets to Na.Reactive r (Na.Event r a)
+--  -- gets to Reactive r (Na.Event r a)
 --  x >>= f = wrap . join . fmap (join . fmap (Na.switchE . fmap (maybe Na.never id))
 --    . unwrap . fromEvent . Na.execute . Na.value . fmap (fmap Na.value . unwrap . f)) . unwrap $ x
 ----
@@ -45,36 +45,36 @@ type Event r a = Behaviour r (Maybe a)
 
 -- Helpers
 
-unwrap :: Behaviour r a -> Na.Reactive r (Na.Behaviour r a)
+unwrap :: Behaviour r a -> Reactive r (Na.Behaviour r a)
 unwrap (Behaviour r) = r
 
-wrap :: Na.Reactive r (Na.Behaviour r a) -> Behaviour r a
+wrap :: Reactive r (Na.Behaviour r a) -> Behaviour r a
 wrap = Behaviour
 
 -- General map from a Behaviour to an Event.
-behaviourToEvent :: (Na.Context r) => (Na.Event r a -> Na.Event r b) -> Behaviour r a -> Event r b
+behaviourToEvent :: (Context r) => (Na.Event r a -> Na.Event r b) -> Behaviour r a -> Event r b
 behaviourToEvent f = wrap . join . fmap (unwrap . fromEvent . f . Na.value) . unwrap
 
 
 -- Conversions to/from Sodium. The key is that, in the Reactive monad,
 -- 'Event a' is isomorphic to 'Behaviour (Maybe a)'.
 
-fromBehaviour :: (Na.Context r) => Na.Behaviour r a -> Behaviour r a
+fromBehaviour :: (Context r) => Na.Behaviour r a -> Behaviour r a
 fromBehaviour = wrap . return
 
-toBehaviour :: (Na.Context r) => Behaviour r a -> Na.Reactive r (Na.Behaviour r a)
+toBehaviour :: (Context r) => Behaviour r a -> Reactive r (Na.Behaviour r a)
 toBehaviour = unwrap
 
-fromEvent :: (Na.Context r) => Na.Event r a -> Event r a
+fromEvent :: (Context r) => Na.Event r a -> Event r a
 fromEvent = wrap . Na.hold Nothing . fmap Just
 
-toEvent :: (Na.Context r) => Event r a -> Na.Reactive r (Na.Event r a)
+toEvent :: (Context r) => Event r a -> Reactive r (Na.Event r a)
 toEvent = fmap (Na.filterJust . Na.value) . unwrap
 
 
 -- Equivalent core functions
 
-instance (Na.Context r) => Monoid (Event r a) where
+instance (Context r) => Monoid (Event r a) where
   mempty = fromEvent Na.never
   mappend rx ry = wrap $ do
     x <- toEvent rx
@@ -82,28 +82,28 @@ instance (Na.Context r) => Monoid (Event r a) where
     unwrap . fromEvent $ Na.merge x y
 --
 
-never :: (Na.Context r) => Event r a
+never :: (Context r) => Event r a
 never = mempty
 
-merge :: (Na.Context r) => Event r a -> Event r a -> Event r a
+merge :: (Context r) => Event r a -> Event r a -> Event r a
 merge = mappend
 
-filterJust :: (Na.Context r) => Event r (Maybe a) -> Event r a
+filterJust :: (Context r) => Event r (Maybe a) -> Event r a
 filterJust = wrap . fmap (fmap join) . unwrap
 
-hold :: (Na.Context r) => a -> Event r a -> Behaviour r a
+hold :: (Context r) => a -> Event r a -> Behaviour r a
 hold x = wrap . fmap (fmap (maybe x id)) . unwrap
 
-updates :: (Na.Context r) => Behaviour r a -> Event r a
+updates :: (Context r) => Behaviour r a -> Event r a
 updates = wrap . (unwrap . fromEvent . Na.updates =<<) . unwrap
 
-value :: (Na.Context r) => Behaviour r a -> Event r a
+value :: (Context r) => Behaviour r a -> Event r a
 value = wrap . (unwrap . fromEvent . Na.value =<<) . unwrap
 
-snapshot :: (Na.Context r) => (a -> b -> c) -> Event r a -> Behaviour r b -> Event r c
+snapshot :: (Context r) => (a -> b -> c) -> Event r a -> Behaviour r b -> Event r c
 snapshot f e b = liftA2 f <$> e <*> fmap Just b
 
-switchE :: (Na.Context r) => Behaviour r (Event r a) -> Event r a
+switchE :: (Context r) => Behaviour r (Event r a) -> Event r a
 switchE b = let
   f x = (unwrap . fromEvent . Na.switchE =<<)
       . join
@@ -112,7 +112,7 @@ switchE b = let
   in wrap $ sample b >>= toEvent >>= flip f b
 --
 
-switch :: (Na.Context r) => Behaviour r (Behaviour r a) -> Behaviour r a
+switch :: (Context r) => Behaviour r (Behaviour r a) -> Behaviour r a
 switch b = let
   f x = (Na.switch =<<)
       . join
@@ -121,32 +121,32 @@ switch b = let
   in wrap $ sample b >>= toBehaviour >>= flip f b
 --
 
-execute :: (Na.Context r) => Event r (Na.Reactive r a) -> Event r a
+execute :: (Context r) => Event r (Reactive r a) -> Event r a
 execute = wrap . join . fmap (unwrap . fromEvent . Na.execute) . toEvent
 
-sample :: (Na.Context r) => Behaviour r a -> Na.Reactive r a
+sample :: (Context r) => Behaviour r a -> Reactive r a
 sample = join . fmap Na.sample . unwrap
 
-coalesce :: (Na.Context r) => (a -> a -> a) -> Event r a -> Event r a
+coalesce :: (Context r) => (a -> a -> a) -> Event r a -> Event r a
 coalesce f = fmap join . behaviourToEvent (Na.coalesce (liftA2 f))
 
-once :: (Na.Context r) => Event r a -> Event r a
+once :: (Context r) => Event r a -> Event r a
 once = fmap join . behaviourToEvent Na.once
 
-split :: (Na.Context r) => Event r [a] -> Event r a
+split :: (Context r) => Event r [a] -> Event r a
 split = behaviourToEvent (Na.split . fmap (maybe [] id))
 
 
 -- Equivalent derived functions
 
-newBehaviour :: (Na.Context r) => a -> Na.Reactive r (Behaviour r a, a -> Na.Reactive r ())
+newBehaviour :: (Context r) => a -> Reactive r (Behaviour r a, a -> Reactive r ())
 newBehaviour = (return . (fromBehaviour . fst &&& snd) =<<) . Na.newBehaviour
 
 -- issue: 'set (pure a)' is never propagated, only 'set empty' is. Not
 --        sure if this is an issue with Sodium or not.
-newEvent :: (Na.Context r) => Na.Reactive r (Event r a, a -> Na.Reactive r ())
+newEvent :: (Context r) => Reactive r (Event r a, a -> Reactive r ())
 newEvent = let
-  pulse :: (Na.Context r, Alternative f) => (f a -> Na.Reactive r ()) -> a -> Na.Reactive r ()
+  pulse :: (Context r, Alternative f) => (f a -> Reactive r ()) -> a -> Reactive r ()
   pulse set a = set (pure a) >> set empty
   in return . (fmap join . fromEvent . fst &&& pulse . snd) =<< Na.newEvent
 --
